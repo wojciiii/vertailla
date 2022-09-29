b852fb22837f4d1c92a276e804ed4c7cdbf8c9fa
Michael Wojciechowski <wojciii@gmail.com>
2022-09-29 22:20:25 +0200

    Image loading and finding section inside mmap'ed bin file.

---
+++ src/image.py
@@ -0,0 +1,47 @@
+from intelhex import IntelHex
+import os
+
+BIN_EXT = "bin"
+INTEL_HEX_EXT = "hex"
+
+
+def align_up(num, align):
+    assert (align & (align - 1) == 0) and align != 0
+    return (num + (align - 1)) & ~(align - 1)
+
+
+class Image:
+
+    def __init__(self):
+        self.payload = []
+        self.base_addr = 0
+
+    def load(self, path):
+        """Load an image from a given file"""
+        ext = os.path.splitext(path)[1][1:].lower()
+        try:
+            if ext == INTEL_HEX_EXT:
+                ih = IntelHex(path)
+                self.payload = ih.tobinarray()
+                self.base_addr = ih.minaddr()
+            else:
+                with open(path, 'rb') as f:
+                    self.payload = f.read()
+        except FileNotFoundError as e:
+            raise e
+
+    def get_base_address(self):
+        return self.base_addr
+
+    def save(self, path):
+        """Save an image from a given file"""
+        ext = os.path.splitext(path)[1][1:].lower()
+        if ext == INTEL_HEX_EXT:
+            # input was in binary format, but HEX needs to know the base addr
+            h = IntelHex()
+            h.frombytes(bytes=self.payload, offset=self.base_addr)
+            h.tofile(path, 'hex')
+        else:
+            with open(path, 'wb') as f:
+                f.write(self.payload)
+
--- src/test.py
+++ src/test.py
@@ -2,20 +2,45 @@
 import sys
 from elftools.elf.elffile import ELFFile
 from elftools.elf.segments import Segment
+import mmap
+from image import Image
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("You must provide this script with an elf binary file you want to examine")
-        sys.exit(1)
+input_elf = sys.argv[1]
+input_hex = sys.argv[2]
+
+if len(sys.argv) < 3:
+    print("Usage: script <elf> <hex>")
+    #print("You must provide this script with an elf binary file you want to examine")
+    sys.exit(1)
+
+# Temp directory:
+TEMPDIR = "./temp"
+
+i = Image()
+i.load(input_hex)
+ba = i.get_base_address()
+temp_file = TEMPDIR + "/" + "input.bin"
+i.save(temp_file)
+
+print(f"Loaded file {input_hex} with base address {hex(ba)}")
+
+with open(temp_file, "r+b") as f:
+    mm = mmap.mmap(f.fileno(), 0)
+    #print(mm.find(b'\x00\x09\x03\x03'))
+
+    #with open(sys.argv[2], "r") as f:
+    #    mm = mmap.mmap(f.fileno(), 0)
+    #    print(mm.find('\x00\x09\x03\x03'))
 
     segments_of_interrest = []
     with open(sys.argv[1], 'rb') as elffile:
         for segment in ELFFile(elffile).iter_segments():
             if segment.header.p_type == "PT_LOAD":
-                print(segment.header)
+                #print(segment.header)
                 segments_of_interrest.append(segment)
 
-        wanted_sections = ["SHT_PROGBITS", "SHT_NOBITS"]
+        #wanted_sections = ["SHT_PROGBITS", "SHT_NOBITS"]
+        wanted_sections = ["SHT_PROGBITS"]
         for section in ELFFile(elffile).iter_sections():
             found = False
             segment_name = ""
@@ -27,10 +52,14 @@ if __name__ == '__main__':
                     break
                 segment_count += 1
             if found and section.header['sh_type'] in wanted_sections:
-                print(f"Segment: {segment_name}, section: {section.name}, type: {section.header['sh_type']}, data size: {section.data_size}")
-                for b in section.data():
-                    print(hex(b))
+                print(f"Segment: {segment_name}, section: {section.name}, type: {section.header['sh_type']}, data size: {section.data_size}, offset: ???")
                 print(len(section.data()))
+                if len(section.data()) == 32:
+                    print(section.data())
+                data_found = (mm.find(section.data()) != -1)
+                print(f"Data found: {data_found}")
+                #sys.exit(0)
+
                 #n = section.num_symbols()
                 #for i in range(0, n):
                 #    print(f"Symbol: {i}")
